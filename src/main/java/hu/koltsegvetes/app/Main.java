f9e96702e309c048dbed8fc8646a4238c8841bc2
zkapitany <kapitany.z@gmail.com>
2026-04-10 14:33:19 +0200

    Create Main.java for Budget Calculator application

---
+++ src/main/java/hu/koltsegvetes/app/Main.java
@@ -0,0 +1,8 @@
+// Main class for the budget calculator application
+package hu.koltsegvetes.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Budget Calculator!");
+    }
+}
